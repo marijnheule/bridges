60bfd785c22fc9a469f39ac2db765cab7ec1a381
Marijn Heule <marijn@br011.ib.bridges2.psc.edu>
2021-08-28 14:20:09 -0400

    kissat

--- pKissat.sh
+++ pKissat.sh
@@ -7,6 +7,6 @@ mkdir ~/log/
 for i in {1..128}
 do
   R=$RANDOM$RANDOM
-  scranfilize -s $R -f 0 -v 0 -c 1 $CNF | cadical $2 $3 $4 $5 $6 $7 $8 $9 > ~/log/$BASE-C-$R.log &
+  scranfilize -s $R -f 0 -v 0 -c 1 $CNF | kissat $2 $3 $4 $5 $6 $7 $8 $9 > ~/log/$BASE-K-$R.log &
 done
 wait
