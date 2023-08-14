4b026563278c18c45d4819ee5aa0ba82700b6ca5
Marijn Heule <marijn@br013.ib.bridges2.psc.edu>
2023-08-14 08:53:35 -0400

    aKissat

---
+++ aKissat.sh
@@ -0,0 +1,16 @@
+ICNF=$1
+SPLIT=128
+BASE=${ICNF%.*}
+DIR=$2
+
+for (( i=0; i<$SPLIT; i++ ))
+do
+  cat $ICNF | awk '!/a / {print $0} /a / {if ((NR % '$SPLIT') == '$i') print $0}' > $DIR/$BASE-x$i.icnf
+done
+
+for (( i=0; i<$SPLIT; i++ ))
+do
+  INIT=$(($i+1))
+  ~/bridges/kissat-assume.sh $DIR/$BASE-x$i.icnf $INIT $SPLIT &
+done
+wait
